acc9da5d7eafef314e272a2fef29a97fd49160ab
Adiel-G <adieltavarez@gmail.com>
2025-10-19 20:43:19 -0400

    add email format validation to user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true, trim: true},
-    email: {type: String, required: true, unique: true, trim: true, lowercase: true},
+    email: {type: String, required: true, unique: true, trim: true, lowercase: true, match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/},
     password: {type: String, required: true, minlength: 8, select: false},
     
     // Profile details
