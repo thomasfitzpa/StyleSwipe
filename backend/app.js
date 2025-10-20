3dfc1a048716270051d22902c12b4d1f03a34e97
Adiel-G <adieltavarez@gmail.com>
2025-10-19 20:33:43 -0400

    add database connection to app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import connectDB from './config/db.js';
 
 const app = express();
 
 app.use(express.json());
+
+// Connect to database
+connectDB();
---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export default connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Database connection failed: ${error.message}`);
+        process.exit(1);
+    }
+};
