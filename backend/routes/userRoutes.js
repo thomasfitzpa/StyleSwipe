bb93f2ec7ac679fa4b9f73267a78fa8c2e3c741f
Adiel-G <adieltavarez@gmail.com>
2025-10-21 20:20:06 -0400

    add login, token, and logout routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { validateUserCreation } from '../validator/userValidator.js';
-import { register } from '../controllers/userController.js';
+import { validateUserCreation, validateUserLogin } from '../validator/userValidator.js';
+import { register, login, refresh, logout } from '../controllers/userController.js';
 
 const userRouter = Router();
 
 userRouter.post('/register', validateUserCreation, register);
+userRouter.post('/login', validateUserLogin, login);
+userRouter.post('/token', refresh);
+userRouter.post('/logout', logout);
 
 export default userRouter;
