b8589ef72e37b542b3482591ad421b5ae8d9fe3c
Adiel-G <adieltavarez@gmail.com>
2025-10-21 20:18:56 -0400

    add custom unauthorized and forbidden errors

--- backend/errors/errors.js
+++ backend/errors/errors.js
@@ -6,6 +6,18 @@ export class BadRequestError extends CustomError {
     }
 }
 
+export class UnauthorizedError extends CustomError {
+    constructor(message = 'Unauthorized') {
+        super(message, 401)
+    }
+}
+
+export class ForbiddenError extends CustomError {
+    constructor(message = 'Forbidden') {
+        super(message, 403)
+    }
+}
+
 export class NotFoundError extends CustomError {
     constructor(message = 'Not Found') {
         super(message, 404)
